6817d3a84649ce019a98428c68ef45764bee482c
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-01-29 21:37:38 +0900

    Example 7-10

---
+++ class/7-10.js
@@ -0,0 +1,19 @@
+var Rectangle = function (width, height) {
+  this.width = width;
+  this.height = height;
+};
+
+Rectangle.prototype.getArea = function () {
+  return this.width * this.height;
+};
+
+var Square = function (width) {
+  Rectangle.call(this, width, width);
+};
+
+Square.prototype = Object.create(Rectangle.prototype);
+Object.freeze(Square.prototype);
+
+var s = new Square(5);
+console.log(s);
+console.log(s.getArea());
