d0f5a7c9acccd24949d640234f26067f513f7744
cho-hadam <s2019w16@e-mirim.hs.kr>
2021-01-30 13:19:46 +0900

    Example 7-16

---
+++ class/7-16.js
@@ -0,0 +1,23 @@
+var Rectangle = class {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getArea() {
+    return this.width * this.height;
+  }
+};
+
+var Square = class extends Rectangle {
+  constructor(width) {
+    super(width, width);
+  }
+
+  getArea() {
+    console.log("size is", super.getArea());
+  }
+};
+
+var s = new Square(11);
+s.getArea();
